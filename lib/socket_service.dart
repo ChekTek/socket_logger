f0532072a53535fc823d9caf993e697567d6c191
Zack Hoherchak <zachary.hoherchak@corsair.com>
2022-02-09 01:21:52 -0500

    fixed socket channels. TODO: Fix change detection

--- lib/socket_service.dart
+++ lib/socket_service.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:shelf_web_socket/shelf_web_socket.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+// import 'package:web_socket_channel/web_socket_channel.dart';
+
 import 'log.dart';
 
 class SocketService {
@@ -23,34 +27,57 @@ class SocketService {
 
     host = socket;
 
-    try {
-      var socketInfo = host.split(':');
-      server = await ServerSocket.bind(
-        socketInfo[0],
-        int.parse(socketInfo[1]),
-      );
-
-      server.listen((Socket client) {
-        client.listen(
-          (data) {
-            String result = String.fromCharCodes(data);
-            _controller.sink.add(result);
-          },
-          onError: (error) {
-            client.close();
-            client.destroy();
-          },
-          onDone: () {
-            client.close();
-            client.destroy();
-          },
-        );
+    var handler = webSocketHandler((webSocket) {
+      // (webSocket as WebSocketChannel)
+      print(webSocket);
+      webSocket.stream.listen((message) {
+        _controller.sink.add(message);
+        print(message);
       });
-      connected = true;
-      connecting = false;
-    } catch (e) {
-      print(e);
-    }
+    });
+
+    shelf_io.serve(handler, 'localhost', 6969).then((server) {
+      print('Serving at ws://${server.address.host}:${server.port}');
+    });
+
+    // runZoned(() async {
+    //   var server = await HttpServer.bind('127.0.0.1', 6969);
+    //   server.listen((HttpRequest req) async {
+    //     if (req.uri.path == '/ws') {
+    //       var socket = await WebSocketTransformer.upgrade(req);
+    //       socket.listen(print);
+    //     }
+    //   });
+    // }, onError: (e) => print("An error occurred."));
+
+    // try {
+    //   var socketInfo = host.split(':');
+    //   server = await ServerSocket.bind(
+    //     socketInfo[0],
+    //     int.parse(socketInfo[1]),
+    //   );
+
+    //   server.listen((Socket client) {
+    //     client.listen(
+    //       (data) {
+    //         String result = String.fromCharCodes(data);
+    //         _controller.sink.add(result);
+    //       },
+    //       onError: (error) {
+    //         client.close();
+    //         client.destroy();
+    //       },
+    //       onDone: () {
+    //         client.close();
+    //         client.destroy();
+    //       },
+    //     );
+    //   });
+    //   connected = true;
+    //   connecting = false;
+    // } catch (e) {
+    //   print(e);
+    // }
   }
 
   disconnect() {
